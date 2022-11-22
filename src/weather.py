c854231375812591788d5de3fc0e9760c6f5e98b
Giancarmelo <giancarmelo99@gmail.com>
2022-11-22 09:41:15 +0100

    fix: return always array type

--- src/weather.py
+++ src/weather.py
@@ -11,8 +11,7 @@ class WeatherCall:
 
     def get_coordinates(self, city):
         response = requests.get(f"https://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={self.key}")
-        if(response.json() == []): return None
-        else: return response.json()
+        return response.json()
 
     def get_weather(self, lat, lon):
         response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={self.key}")
