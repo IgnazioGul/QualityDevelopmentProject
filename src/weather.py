1681cc6bdf17c83e3b0e248bf11342b6aa033ae3
weelics <smaugeri97@gmail.com>
2022-11-22 15:15:08 +0100

    chore: update weather

--- src/weather.py
+++ src/weather.py
@@ -1,12 +1,11 @@
 import requests
 from dotenv import load_dotenv
 import os
-load_dotenv()
 
+load_dotenv()
 OPEN_WEATHER_CHECK = os.getenv('OPEN_WEATHER_CHECK')
 OPEN_WEATHER_GEO = os.getenv('OPEN_WEATHER_GEO')
 OPEN_WEATHER_CITY = os.getenv('OPEN_WEATHER_CITY')
-
 
 class WeatherCall:
 
@@ -17,7 +16,7 @@ class WeatherCall:
         response = requests.get(OPEN_WEATHER_CHECK + self.key)
         if(response.status_code == 401): raise TypeError("invalid Key")
 
-    def get_coordinates(self, city, limit):
+    def get_coordinates(self, city, limit = 5):
         response = requests.get(f"{OPEN_WEATHER_GEO}q={city}&limit={limit}&appid={self.key}")
         if(response.status_code == 401): raise TypeError("error call")
         return response.json()
