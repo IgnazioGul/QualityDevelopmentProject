1c3a6009db8aa16e008ba24088c11fd337e19ebc
weelics <smaugeri97@gmail.com>
2022-11-22 16:03:02 +0100

    chore: update weather

--- src/weather.py
+++ src/weather.py
@@ -3,25 +3,34 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 OPEN_WEATHER_CHECK = os.getenv('OPEN_WEATHER_CHECK')
 OPEN_WEATHER_GEO = os.getenv('OPEN_WEATHER_GEO')
 OPEN_WEATHER_CITY = os.getenv('OPEN_WEATHER_CITY')
 
 class WeatherCall:
 
-    def __init__(self, key):
+    def __init__( self, key: str ) -> None:
+        """costruct""" 
         self.key = key
 
-    def checkKey(self) -> None:
-        response = requests.get(OPEN_WEATHER_CHECK + self.key)
-        if(response.status_code == 401): raise TypeError("invalid Key")
+    def check_key( self ) -> None:
+        """check the api key"""
+        response = requests.get( OPEN_WEATHER_CHECK + self.key, timeout=10 )
+        if response.status_code == 401: 
+            raise TypeError( "Invalid Key" )
 
-    def get_coordinates(self, city: str, limit: int) -> dict:
-        response = requests.get(f"{OPEN_WEATHER_GEO}q={city}&limit={limit}&appid={self.key}")
-        if(response.status_code == 401): raise TypeError("error call")
+    def get_coordinates( self, city: str, limit: int ) -> dict:
+        """get coordinates by city name"""
+        response = requests.get( f"{OPEN_WEATHER_GEO}q={city}&limit={limit}&appid={self.key}", timeout=10 )
+        if response.status_code == 401: 
+            raise TypeError( "Error Call" )
         return response.json()
 
-    def get_weather(self, lat: float, lon: float) -> dict:
-        response = requests.get(f"{OPEN_WEATHER_CITY}lat={lat}&lon={lon}&appid={self.key}")
-        if(response.status_code == 401): raise TypeError("error call")
+    def get_weather( self, lat: float, lon: float ) -> dict:
+        """get weather by latitune and longitudine"""
+        response = requests.get( f"{OPEN_WEATHER_CITY}lat={lat}&lon={lon}&appid={self.key}", timeout=10 )
+        if response.status_code == 401: 
+            raise TypeError( "Error Call" )
         return response.json()
+        
