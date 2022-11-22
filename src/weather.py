df27a92e492b031cdbe54e6a9b9c871f58537c77
weelics <smaugeri97@gmail.com>
2022-11-22 16:14:05 +0100

    chore: update weather

--- src/weather.py
+++ src/weather.py
@@ -8,25 +8,27 @@ OPEN_WEATHER_CHECK = os.getenv('OPEN_WEATHER_CHECK')
 OPEN_WEATHER_GEO = os.getenv('OPEN_WEATHER_GEO')
 OPEN_WEATHER_CITY = os.getenv('OPEN_WEATHER_CITY')
 
+
 class WeatherCall:
 
-    def __init__( self, key: str ) -> None:
+    def __init__(self, key: str) -> None:
         self.key = key
 
-    def check_key( self ) -> None:
-        response = requests.get( OPEN_WEATHER_CHECK + self.key, timeout=10 )
-        if response.status_code == 401: 
-            raise TypeError( "Invalid Key" )
+    def check_key(self) -> None:
+        response = requests.get(OPEN_WEATHER_CHECK + self.key, timeout=10)
+        if response.status_code == 401:
+            raise TypeError("Invalid Key")
 
-    def get_coordinates( self, city: str, limit: int ) -> dict:
-        response = requests.get( f"{OPEN_WEATHER_GEO}q={city}&limit={limit}&appid={self.key}", timeout=10 )
-        if response.status_code == 401: 
-            raise TypeError( "Error Call" )
+    def get_coordinates(self, city: str, limit: int) -> dict:
+        response = requests.get(
+            f"{OPEN_WEATHER_GEO}q={city}&limit={limit}&appid={self.key}", timeout=10)
+        if response.status_code == 401:
+            raise TypeError("Error Call")
         return response.json()
 
-    def get_weather( self, lat: float, lon: float ) -> dict:
-        response = requests.get( f"{OPEN_WEATHER_CITY}lat={lat}&lon={lon}&appid={self.key}", timeout=10 )
-        if response.status_code == 401: 
-            raise TypeError( "Error Call" )
+    def get_weather(self, lat: float, lon: float) -> dict:
+        response = requests.get(
+            f"{OPEN_WEATHER_CITY}lat={lat}&lon={lon}&appid={self.key}", timeout=10)
+        if response.status_code == 401:
+            raise TypeError("Error Call")
         return response.json()
-        
