7491af9dfa3ecf7104ba338e8526cd41dddc0c64
weelics <smaugeri97@gmail.com>
2022-11-25 00:14:57 +0100

    fix: upgrade weather and test_weather

--- src/weather.py
+++ src/weather.py
@@ -1,40 +1,60 @@
-from src.call import check,geo,weather
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+OPEN_WEATHER_KEY = os.getenv('OPEN_WEATHER_KEY')
+OPEN_WEATHER_CHECK = os.getenv('OPEN_WEATHER_CHECK')
+OPEN_WEATHER_GEO = os.getenv('OPEN_WEATHER_GEO')
+OPEN_WEATHER_CITY = os.getenv('OPEN_WEATHER_CITY')
 
 class WeatherCall:
 
-    def __init__(self, key: str) -> None:
-        self.key = key
-
-    def check_key(self) -> None:
+    def check_key(self) -> str | TypeError:
         """
         check_key method checks if the key is correct for the API call
         :return:  if the error occurs, the method returns an exception
         """
-        response = check(self.key)
+        response = self.check(OPEN_WEATHER_KEY)
         if response == 401:
             raise TypeError("Invalid Key")
+        return response
 
-    def get_coordinates(self, city: str, limit: int) -> dict | None:
+    def get_coordinates(self, city: str, limit: int) -> dict | TypeError:
         """
         the get_coordinates method executes an API call which, given a city, returns its coordinates
         :param city: the city taken to have its coordinates
         :param limit:the maximum number of values chosen
         :return: the json containing the coordinates
         """
-        response = geo(self.key, city, limit)
+        response = self.geo_response(OPEN_WEATHER_KEY, city, limit)
         if "cod" in response:
             raise TypeError("Error Call")
         return response
 
-    def get_weather(self, lat: float, lon: float) -> dict | None:
+    def get_weather(self, lat: float, lon: float) -> dict | TypeError:
         """
         the get_weather method executes an API call which, given latitude and longitude, returns its weather
         :param lat: latitude
         :param lon: longitude
         :return: the json containing all the information on the weather of the city
         """
-        response = weather(self.key, lat, lon)
-        if "cod" in response and response['cod'] == 401:
+        response = self.weather_response(OPEN_WEATHER_KEY, lat, lon)
+        if "cod" in response and response['cod'] != 200:
             raise TypeError("Error Call")
         return response
-        
+
+    @staticmethod
+    def check(key: str) -> str:
+        res = requests.get(OPEN_WEATHER_CHECK + key, timeout=10).json()['cod']
+        return res
+
+    @staticmethod
+    def geo_response(key: str, city: str, limit: int) -> dict:
+        res = requests.get(f"{OPEN_WEATHER_GEO}q={city}&limit={limit}&appid={key}", timeout=10).json()
+        return res
+
+    @staticmethod
+    def weather_response(key: str, lat: float, lon: float) -> dict:
+        res = requests.get(f"{OPEN_WEATHER_CITY}lat={lat}&lon={lon}&appid={key}", timeout=10).json()
+        return res
--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,74 +1,55 @@
-from src import call
 from src.weather import WeatherCall
 from pytest_mock import MockerFixture
 import pytest
+import os
+from dotenv import load_dotenv
+import requests
 
+load_dotenv()
+OPEN_WEATHER_KEY = os.getenv('OPEN_WEATHER_KEY')
+
+def test_check_response():
+    w = WeatherCall.check(OPEN_WEATHER_KEY)
+    assert w == '200'
+
+def test_geo_response():
+    roma_json = [{"name":"Rome","local_names":{"nl":"Rome","eu":"Erroma","am":"ሮማ","pt":"Roma","af":"Rome","fy":"Rome","mr":"रोम","cv":"Рим","sg":"Rome","ja":"ローマ","pl":"Rzym","hy":"Հռոմ","kl":"Roma","sq":"Roma","ks":"روم","ia":"Roma","mg":"Roma","id":"Roma","ca":"Roma","ko":"로마","ga":"An Róimh","es":"Roma","th":"โรม","he":"רומא","se":"Roma","ml":"റോം","cu":"Римъ","uz":"Rim","da":"Rom","lb":"Roum","tg":"Рим","bg":"Рим","sr":"Рим","hi":"रोम","my":"ရောမမြို့","mt":"Ruma","mk":"Рим","fr":"Rome","cr":"ᖌᒪ","nn":"Roma","so":"Roma","ie":"Roma","oc":"Roma","gn":"Róma","bi":"Rome","cy":"Rhufain","be":"Рым","sv":"Rom","ne":"रोम","sh":"Rim","mi":"Rōma","os":"Ром","ar":"روما","sa":"रोमा","lv":"Roma","ee":"Rome","tk":"Rim","en":"Rome","ru":"Рим","ty":"Roma","la":"Roma","ka":"რომი","yo":"Rómù","io":"Roma","uk":"Рим","de":"Rom","fi":"Rooma","gd":"An Ròimh","si":"රෝමය","rn":"Roma","vo":"Roma","no":"Roma","sc":"Roma","et":"Rooma","fa":"رم","ku":"Roma","ht":"Ròm","it":"Roma","gv":"Yn Raue","rw":"Roma","ta":"உரோமை நகரம்","tr":"Roma","kk":"Рим","sk":"Rím","te":"రోమ్","qu":"Roma","sw":"Roma","kn":"ರೋಮ","ay":"Roma","su":"Roma","lt":"Roma","bs":"Rim","kv":"Рим","is":"Róm","yi":"רוים","rm":"Roma","an":"Roma","gl":"Roma","hr":"Rim","el":"Ρώμη","ur":"روم","li":"Roeme","sl":"Rim","tl":"Roma","ug":"رىم","sd":"روم","az":"Roma","zh":"羅馬/罗马","fo":"Róm","co":"Roma","bo":"རོ་མ།","mn":"Ром","vi":"Rô-ma","tt":"Рим","bn":"রোম","ba":"Рим","br":"Roma","ln":"Roma","hu":"Róma","jv":"Roma","cs":"Řím","ro":"Roma","na":"Roma","eo":"Romo"},"lat":41.8933203,"lon":12.4829321,"country":"IT","state":"Lazio"}]
+    w = WeatherCall.geo_response(OPEN_WEATHER_KEY, 'roma', 1)
+    assert w == roma_json
+
+def test_weather_response():
+    w = WeatherCall.weather_response(OPEN_WEATHER_KEY,41.8933203, 12.4829321)
+    assert 'weather' in w
 
 def test_check_key_passes(mocker):
-    #arrage
-    mock_json_return = 200
-    mocker.patch.object(call, "check", return_value = mock_json_return )
-    m = WeatherCall('test')
-    #assert
-    with pytest.raises(TypeError):
-        #act
-        m.check_key()
-
+    mock_json_return = '200'
+    mocker.patch("src.weather.WeatherCall.check", return_value = mock_json_return )
+    assert WeatherCall.check_key(WeatherCall) == '200'
 
 def test_check_key_fail(mocker):
-    #arrage
     mock_json_return = 401
-    mocker.patch.object(call, "check", return_value = mock_json_return )
-    m = WeatherCall('test')
-    #assert
+    mocker.patch("src.weather.WeatherCall.check", return_value = mock_json_return )
     with pytest.raises(TypeError):
-        #act
-        m.check_key()
-
-
+        WeatherCall.check_key(WeatherCall)
 
 def test_get_coordinates_passes(mocker):
-    #arrage
     mock_json_return = [{"name":"Rome","local_names":{"nl":"Rome","eu":"Erroma","am":"ሮማ","pt":"Roma","af":"Rome","fy":"Rome","mr":"रोम","cv":"Рим","sg":"Rome","ja":"ローマ","pl":"Rzym","hy":"Հռոմ","kl":"Roma","sq":"Roma","ks":"روم","ia":"Roma","mg":"Roma","id":"Roma","ca":"Roma","ko":"로마","ga":"An Róimh","es":"Roma","th":"โรม","he":"רומא","se":"Roma","ml":"റോം","cu":"Римъ","uz":"Rim","da":"Rom","lb":"Roum","tg":"Рим","bg":"Рим","sr":"Рим","hi":"रोम","my":"ရောမမြို့","mt":"Ruma","mk":"Рим","fr":"Rome","cr":"ᖌᒪ","nn":"Roma","so":"Roma","ie":"Roma","oc":"Roma","gn":"Róma","bi":"Rome","cy":"Rhufain","be":"Рым","sv":"Rom","ne":"रोम","sh":"Rim","mi":"Rōma","os":"Ром","ar":"روما","sa":"रोमा","lv":"Roma","ee":"Rome","tk":"Rim","en":"Rome","ru":"Рим","ty":"Roma","la":"Roma","ka":"რომი","yo":"Rómù","io":"Roma","uk":"Рим","de":"Rom","fi":"Rooma","gd":"An Ròimh","si":"රෝමය","rn":"Roma","vo":"Roma","no":"Roma","sc":"Roma","et":"Rooma","fa":"رم","ku":"Roma","ht":"Ròm","it":"Roma","gv":"Yn Raue","rw":"Roma","ta":"உரோமை நகரம்","tr":"Roma","kk":"Рим","sk":"Rím","te":"రోమ్","qu":"Roma","sw":"Roma","kn":"ರೋಮ","ay":"Roma","su":"Roma","lt":"Roma","bs":"Rim","kv":"Рим","is":"Róm","yi":"רוים","rm":"Roma","an":"Roma","gl":"Roma","hr":"Rim","el":"Ρώμη","ur":"روم","li":"Roeme","sl":"Rim","tl":"Roma","ug":"رىم","sd":"روم","az":"Roma","zh":"羅馬/罗马","fo":"Róm","co":"Roma","bo":"རོ་མ།","mn":"Ром","vi":"Rô-ma","tt":"Рим","bn":"রোম","ba":"Рим","br":"Roma","ln":"Roma","hu":"Róma","jv":"Roma","cs":"Řím","ro":"Roma","na":"Roma","eo":"Romo"},"lat":41.8933203,"lon":12.4829321,"country":"IT","state":"Lazio"}]
-    mocker.patch.object(call, "geo", return_value = mock_json_return )
-    m = WeatherCall('test')
-    #assert
-    with pytest.raises(TypeError):
-        #act
-        assert m.get_coordinates('roma', 1) == mock_json_return
-
-
+    mocker.patch("src.weather.WeatherCall.geo_response", return_value = mock_json_return )
+    assert WeatherCall.get_coordinates(WeatherCall,'roma', 1) == mock_json_return
 
 def test_get_coordinates_fail(mocker):
-    #arrage
     mock_json_return = {"cod":401, "message": "Invalid API key. Please see https://openweathermap.org/faq#error401 for more info."}
-    mocker.patch.object(call, "geo", return_value = mock_json_return )
-    m = WeatherCall('test')
-    #assert
+    mocker.patch("src.weather.WeatherCall.geo_response", return_value = mock_json_return )
     with pytest.raises(TypeError):
-        #act
-        m.get_coordinates('roma', 1)
-
+        WeatherCall.get_coordinates(WeatherCall,'roma', 1)
 
 def test_get_weather_passes(mocker):
-    #arrage
     mock_json_return = {"coord":{"lon":12.4829,"lat":41.8933},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":285.51,"feels_like":284.76,"temp_min":283.38,"temp_max":287.2,"pressure":1006,"humidity":75},"visibility":10000,"wind":{"speed":1.03,"deg":110},"clouds":{"all":0},"dt":1669221304,"sys":{"type":2,"id":2000926,"country":"IT","sunrise":1669183751,"sunset":1669218241},"timezone":3600,"id":3169070,"name":"Rome","cod":200}
-    mocker.patch.object(call, "weather", return_value = mock_json_return )
-    m = WeatherCall('test')
-    #assert
-    with pytest.raises(TypeError):
-        #act
-        m.get_weather(41.8933203, 12.4829321)
-
-
+    mocker.patch("src.weather.WeatherCall.weather_response", return_value = mock_json_return )
+    assert WeatherCall.get_weather(WeatherCall,41.8933203, 12.4829321) == mock_json_return
 
 def test_get_weather_fail(mocker):
-    #arrage
     mock_json_return = {"cod":401, "message": "Invalid API key. Please see https://openweathermap.org/faq#error401 for more info."}
-    mocker.patch.object(call, "weather", return_value = mock_json_return )
-    m = WeatherCall('test')
-    #assert
+    mocker.patch("src.weather.WeatherCall.weather_response", return_value = mock_json_return )
     with pytest.raises(TypeError):
-        #act
-        m.get_weather(41.8933203, 12.4829321)
-        
+        WeatherCall.get_weather(WeatherCall,41.8933203, 12.4829321)
