f97497ecb81b53af66c691306bf193b7bd795c54
weelics <smaugeri97@gmail.com>
2022-11-28 14:48:20 +0100

    chore: upgrade test weather

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,26 +1,6 @@
 from src.weather import WeatherCall
 from pytest_mock import MockerFixture
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-OPEN_WEATHER_KEY = os.getenv('OPEN_WEATHER_KEY')
-
-
-'''
-def test_check_response():
-    w = WeatherCall.check(OPEN_WEATHER_KEY)
-    assert w == '200'
-
-def test_geo_response():
-    roma_json = [{"name":"Rome","local_names":{"nl":"Rome","eu":"Erroma","am":"ሮማ","pt":"Roma","af":"Rome","fy":"Rome","mr":"रोम","cv":"Рим","sg":"Rome","ja":"ローマ","pl":"Rzym","hy":"Հռոմ","kl":"Roma","sq":"Roma","ks":"روم","ia":"Roma","mg":"Roma","id":"Roma","ca":"Roma","ko":"로마","ga":"An Róimh","es":"Roma","th":"โรม","he":"רומא","se":"Roma","ml":"റോം","cu":"Римъ","uz":"Rim","da":"Rom","lb":"Roum","tg":"Рим","bg":"Рим","sr":"Рим","hi":"रोम","my":"ရောမမြို့","mt":"Ruma","mk":"Рим","fr":"Rome","cr":"ᖌᒪ","nn":"Roma","so":"Roma","ie":"Roma","oc":"Roma","gn":"Róma","bi":"Rome","cy":"Rhufain","be":"Рым","sv":"Rom","ne":"रोम","sh":"Rim","mi":"Rōma","os":"Ром","ar":"روما","sa":"रोमा","lv":"Roma","ee":"Rome","tk":"Rim","en":"Rome","ru":"Рим","ty":"Roma","la":"Roma","ka":"რომი","yo":"Rómù","io":"Roma","uk":"Рим","de":"Rom","fi":"Rooma","gd":"An Ròimh","si":"රෝමය","rn":"Roma","vo":"Roma","no":"Roma","sc":"Roma","et":"Rooma","fa":"رم","ku":"Roma","ht":"Ròm","it":"Roma","gv":"Yn Raue","rw":"Roma","ta":"உரோமை நகரம்","tr":"Roma","kk":"Рим","sk":"Rím","te":"రోమ్","qu":"Roma","sw":"Roma","kn":"ರೋಮ","ay":"Roma","su":"Roma","lt":"Roma","bs":"Rim","kv":"Рим","is":"Róm","yi":"רוים","rm":"Roma","an":"Roma","gl":"Roma","hr":"Rim","el":"Ρώμη","ur":"روم","li":"Roeme","sl":"Rim","tl":"Roma","ug":"رىم","sd":"روم","az":"Roma","zh":"羅馬/罗马","fo":"Róm","co":"Roma","bo":"རོ་མ།","mn":"Ром","vi":"Rô-ma","tt":"Рим","bn":"রোম","ba":"Рим","br":"Roma","ln":"Roma","hu":"Róma","jv":"Roma","cs":"Řím","ro":"Roma","na":"Roma","eo":"Romo"},"lat":41.8933203,"lon":12.4829321,"country":"IT","state":"Lazio"}]
-    w = WeatherCall.geo_response(OPEN_WEATHER_KEY, 'roma', 1)
-    assert w == roma_json
-
-def test_weather_response():
-    w = WeatherCall.weather_response(OPEN_WEATHER_KEY,41.8933203, 12.4829321)
-    assert 'weather' in w
-'''
 def test_check_key_passes(mocker: MockerFixture):
     '''test the check_key function when the key is correct'''
     #arrange
