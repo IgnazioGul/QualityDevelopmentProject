61f37616a026f7fedacf9e86a448bf697466fda9
Giancarmelo pittala <giancarmelo.pittala@gmail.com>
2022-12-19 10:27:36 +0100

    chore: create message service logic

--- src/services/index.py
+++ src/services/index.py
@@ -43,3 +43,53 @@ async def get_city_by_query(query):
         )
 
     return results
+
+
+async def message_service(update: Update):
+    text = update.message.text
+    coordinate = str(text).split(",")
+    meteo = WeatherCall.get_weather(coordinate[0], coordinate[1])
+    message = get_message(meteo)
+
+    await update.message.reply_text(message)
+
+
+meteo_prop = {
+    'coord': {'lon': float, 'lat': float},
+    'weather': [{'id': float, 'main': str, 'description': str, 'icon': str}],
+    'base': str,
+    'main': {'temp': float, 'feels_like': float, 'temp_min': float, 'temp_max': float, 'pressure': float, 'humidity': float},
+    'visibility': float,
+    'wind': {'speed': float, 'deg': float},
+    'clouds': {'all': float},
+    'dt': int,
+    'sys': {'type': int, 'id': int, 'country': str, 'sunrise': int, 'sunset': int},
+    'timezone': int,
+    'id': int,
+    'name': str,
+    'cod': int
+    }
+
+
+
+def get_message(meteo: meteo_prop) -> str:
+    main = meteo['main']
+    message = ""
+    if 'name' in meteo:
+        message += f"Che tempo fa a {meteo['name']}?\n"
+    if 'temp' in main:
+        message += f"temp.: {main['temp']} C° 🌡\n"
+    if 'feels_like' in main:
+        message += f"temp. avvertita: {main['feels_like']} C° 🌡\n"
+    if 'temp_min' in main:
+        message += f"temp. min: {main['temp_min']} C° 🧊\n"
+    if 'temp_max' in main:
+        message += f"temp. max: {main['temp_max']} C° 🔥\n"
+    if 'pressure' in main:
+        message += f"pressione: {main['pressure']}\n"
+    if 'temp_max' in main:
+        message += f"umidità: {main['humidity']}% 💧\n"
+    if 'sea_level' in main:
+        message += f"livello del mare: {main['sea_level']} 🌊\n"
+
+    return message
