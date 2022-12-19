714a431dec45f4b3aaff1f4baca62f420d828ecb
Giancarmelo pittala <giancarmelo.pittala@gmail.com>
2022-12-19 08:31:13 +0100

    feat: create mai app handler

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+import logging
+from dotenv import load_dotenv
+from telegram.ext import CommandHandler, ApplicationBuilder, InlineQueryHandler, MessageHandler, filters
+from src.handlers.index import start, inline_query, message_handler
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# init dot env
+load_dotenv()
+
+# Get env variable
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+def main() -> None:
+    """Run the bot."""
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler('start', start))
+    app.add_handler(InlineQueryHandler(inline_query))
+    app.add_handler(MessageHandler(filters.Regex(r"^(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)$"), message_handler))
+
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
