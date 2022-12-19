19a6152a11aa2e939fa51d69aa5490a31943b458
Giancarmelo pittala <giancarmelo.pittala@gmail.com>
2022-12-19 10:22:36 +0100

    chore: use get_city_by_query function on handle

--- src/services/index.py
+++ src/services/index.py
@@ -1,6 +1,7 @@
-from telegram import InlineKeyboardButton, Update, InlineKeyboardMarkup
-
+from telegram import InputTextMessageContent, InlineQueryResultArticle, InlineKeyboardButton, Update, InlineKeyboardMarkup
+from src.weather import WeatherCall
 import logging
+from uuid import uuid4
 
 # Enable logging
 logging.basicConfig(
@@ -18,3 +19,27 @@ async def start_service(update: Update):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text('Benvenuto in weater_bot\nrimani aggiornato sul meteo con pochi click', reply_markup=reply_markup)
+
+
+async def get_city_by_query(query):
+    results = []
+    locations = None
+    try:
+        locations = WeatherCall.get_coordinates(query, 5)
+    except TypeError:
+        locations = []
+
+    for location in locations:
+        results.append(
+            InlineQueryResultArticle(
+                id=str(uuid4()),
+                title=f"{ location['name'] if 'name' in location else '' }, { location['state'] if 'state' in location else '' } - { location['country'] if 'state' in location else '' } ",
+                thumb_url="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Weather-sun-clouds-rain.svg/640px-Weather-sun-clouds-rain.svg.png",
+                thumb_width=180,
+                thumb_height=180,
+                input_message_content=InputTextMessageContent(
+                    f"{location['lat']},{location['lon']}"),
+            ),
+        )
+
+    return results
